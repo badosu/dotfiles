68990214ff62b0094021c66d4861a4499c3ece70
Amadeus Folego <amadeusfolego@gmail.com>
2012-01-17 13:04:21 -0200

    xmonad's mocp keybindings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -14,9 +14,16 @@ main = xmonad $ gnomeConfig
 				 , layoutHook = noBorders $ layoutHook defaultConfig
          , startupHook = startup
          }
-				 `additionalKeysP`
-					[ ("M-f", spawn "firefox"),
+				 `additionalKeysP` myKeys
+
+myKeys = [  ("M-f", spawn "firefox"),
 					  ("M-g", spawn "urxvt -e vim"),
 					  ("M-m", spawn "urxvt -e mocp -T transparent-background"),
-						("M-<Return>", spawn "urxvt")
+					  ("M-s", spawn "mocp -s"),
+						("M-<Return>", spawn "urxvt"),
+					  ("C-M1-r", spawn "mocp -r"), 		-- previous
+					  ("C-M1-f", spawn "mocp -f"), 		-- next
+					  ("C-M1-S-p", spawn "mocp -P"),  -- pause
+					  ("C-M1-p", spawn "mocp -p"), 		-- play
+					  ("C-M1-x", spawn "mocp -x")  		-- kill
 					]
