ab281c019e63e039421e54abfcbf0fcbc3ae6f85
Amadeus Folego <amadeusfolego@gmail.com>
2012-01-17 18:54:57 -0200

    Fixed tabs to whitespaces

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,25 +7,25 @@ import XMonad.Util.EZConfig
 startup :: X ()
 startup = do
           spawn "xcompmgr -c" -- Enables compositing
- 
+
 main = xmonad $ gnomeConfig
          { modMask = mod4Mask
          , terminal = "urxvt"
-				 , layoutHook = noBorders $ layoutHook defaultConfig
+         , layoutHook = noBorders $ layoutHook defaultConfig
          , startupHook = startup
          }
-				 `additionalKeysP` myKeys
+         `additionalKeysP` myKeys
 
 myKeys = [  ("M-f", spawn "firefox"),
-					  ("M-g", spawn "urxvt -e vim"),
-					  ("M-m", spawn "urxvt -e mocp -T transparent-background"),
-					  ("M-s", spawn "mocp -s"),
-					  ("M-S-.", spawn "amixer -c 0 set Master 2dB+"),
-					  ("M-S-,", spawn "amixer -c 0 set Master 2dB-"),
-						("M-<Return>", spawn "urxvt"),
-					  ("C-M1-r", spawn "mocp -r"), 		-- previous
-					  ("C-M1-f", spawn "mocp -f"), 		-- next
-					  ("C-M1-S-p", spawn "mocp -P"),  -- pause
-					  ("C-M1-p", spawn "mocp -p"), 		-- play
-					  ("C-M1-x", spawn "mocp -x")  		-- kill
-					]
+            ("M-g", spawn "urxvt -e vim"),
+            ("M-m", spawn "urxvt -e mocp -T transparent-background"),
+            ("M-s", spawn "mocp -s"),
+            ("M-<Return>", spawn "urxvt"),
+            ("M-S-.", spawn "amixer -c 0 set Master 2dB+"), -- raise volume
+            ("M-S-,", spawn "amixer -c 0 set Master 2dB-"), -- lower volume
+            ("C-M1-r", spawn "mocp -r"),     -- previous
+            ("C-M1-f", spawn "mocp -f"),     -- next
+            ("C-M1-S-p", spawn "mocp -P"),   -- pause
+            ("C-M1-p", spawn "mocp -p"),     -- play
+            ("C-M1-x", spawn "mocp -x")      -- kill
+          ]
