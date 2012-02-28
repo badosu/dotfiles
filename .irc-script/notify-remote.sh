73131bdeafbdd4f6fd5542146f519223d735e35f
Amadeus Folego <amadeusfolego@gmail.com>
2012-02-28 14:16:37 -0300

    Adds irc-script

---
+++ .irc-script/notify-remote.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# note: notify-send is required, see libnotify-bin
+
+# XXX do not notify if notification source has focus
+
+delay="4000"
+
+read line
+summary="$line"
+read line
+msg="$line"
+read line
+
+if [ "$line" = "" ] && [ "$summary" != "" ]; then
+    [ -x "$(which notify-send)" ] && notify-send -i notification-message-im -t "$delay" -- "$summary" "$msg"
+fi
