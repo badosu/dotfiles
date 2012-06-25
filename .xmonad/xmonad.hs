603bd6b3e6882133a31884fbbf5fec600581b710
Amadeus Folego <amadeusfolego@gmail.com>
2012-06-25 13:11:28 -0300

    Use mpd and uzbl

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -23,12 +23,10 @@ myManageHook = composeAll [
                             appName =? "Skype" --> doFloat,
                             className =? "xmobar" --> doIgnore,
                             isFullscreen --> doFullFloat,
-                            className =? "Do" --> doIgnore
-                            -- className =? "Unity-2d-launcher" --> doFloat,
-                            -- insistentQuery "xfce4-notifyd" --> doIgnore,
-                            -- appName =? "Synapse" --> doFloat,
-                            -- className =? "Gnome-panel" --> doIgnore,
-                            -- className =? "Unity-2d-panel" --> doIgnore,
+                            className =? "Do" --> doIgnore,
+                            className =? "Unity-2d-panel" --> doIgnore,
+                            className =? "Unity-2d-launcher" --> doFloat,
+                            appName =? "unity-2d-shell" --> doIgnore
                           ]
 
 myLayoutHook = avoidStruts $
@@ -38,32 +36,24 @@ myLayoutHook = avoidStruts $
                layoutHook desktopConfig
 
 myKeys = [ ("M-f", spawn "firefox"),
+           ("M-u", spawn "uzbl-tabbed"),
            ("M-S-q", spawn "gnome-session-quit"),
            ("M-S-r", spawn "xmonad --recompile && xmonad --restart"),
            ("M-v", spawn "urxvt -e vim"),
-           ("M-S-h", moveTo Prev EmptyWS),
-           ("M-S-l", moveTo Next EmptyWS),
-           ("M-S-j", moveTo Prev NonEmptyWS),
-           ("M-S-k", moveTo Next NonEmptyWS),
            ("M-t", spawn "thunderbird"),
-           ("M-h", spawn "nautilus"),
+           ("M-n", spawn "nautilus"),
+           ("M-S-m", spawn "urxvt -e ncmpcpp"),
            ("M-v", spawn "urxvt -e vim"),
            ("M-i", spawn "urxvt -name IRC -e ./.irc-script/irssi-connect.sh"),
            ("M-g", spawn "gvim"),
            ("M-p", spawn "gnome-do"),
-           ("M-m", spawn "urxvt -name Mocp -e mocp -T transparent-background"),
-           ("M-S-.", spawn "amixer -c 0 set Master 2dB+"), -- raise volume
-           ("M-S-,", spawn "amixer -c 0 set Master 2dB-"), -- lower volume
-           ("C-M1-p", spawn "mocp -p"),     -- play
-           ("C-M1-s", spawn "mocp -s"),     -- stop
-           ("C-M1-f", spawn "mocp -f"),     -- next
-           ("C-M1-r", spawn "mocp -r"),     -- previous
-           ("C-M1-S-p", spawn "mocp -P"),   -- pause
-           ("C-M1-S-u", spawn "mocp -U"),   -- unpause
-           ("C-M1-x", spawn "mocp -x"),      -- kill
            ("M-<Return>", spawn "urxvt -name Terminal"),
            ("M-b", sendMessage ToggleStruts),
-           ("M-S-t", withFocused $ windows . W.sink)
+           ("M-S-t", withFocused $ windows . W.sink),
+           ("C-M1-f", spawn "mpc next"),
+           ("C-M1-r", spawn "mpc prev"),
+           ("C-M1-p", spawn "mpc pause"),
+           ("C-M1-S-p", spawn "mpc play")
           ]
 
 myLogHook h = dynamicLogWithPP $ xmobarPP
@@ -73,7 +63,7 @@ myLogHook h = dynamicLogWithPP $ xmobarPP
               }
 
 main = do
-      xmproc <- spawnPipe "~/.cabal/bin/xmobar"
+      xmproc <- spawnPipe "xmobar"
       xmonad $ desktopConfig {
         manageHook = manageDocks <+> myManageHook <+> manageHook desktopConfig,
         layoutHook = myLayoutHook,
@@ -82,3 +72,23 @@ main = do
         terminal = "urxvt",
         modMask = mod4Mask
       } `additionalKeysP` myKeys
+
+-- STORED myKeys
+  --("M-S-h", moveTo Prev NonEmptyWS),
+  --("M-S-l", moveTo Next NonEmptyWS),
+  --("M-S-j", moveTo Prev EmptyWS),
+  --("M-S-k", moveTo Next EmptyWS),
+  --("M-m", spawn "urxvt -name Mocp -e mocp -T transparent-background"),
+  --("M-S-.", spawn "amixer -c 0 set Master 2dB+"), -- raise volume
+  --("M-S-,", spawn "amixer -c 0 set Master 2dB-"), -- lower volume
+  --("C-M1-p", spawn "mocp -p"),     -- play
+  --("C-M1-s", spawn "mocp -s"),     -- stop
+  --("C-M1-f", spawn "mocp -f"),     -- next
+  --("C-M1-r", spawn "mocp -r"),     -- previous
+  --("C-M1-S-p", spawn "mocp -P"),   -- pause
+  --("C-M1-S-u", spawn "mocp -U"),   -- unpause
+  --("C-M1-x", spawn "mocp -x"),      -- kill
+-- STORED myManageHook
+  -- insistentQuery "xfce4-notifyd" --> doIgnore,
+  -- appName =? "Synapse" --> doFloat,
+  -- className =? "Gnome-panel" --> doIgnore,
