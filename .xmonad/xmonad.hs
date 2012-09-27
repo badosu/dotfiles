7bf7c1c5274f0080697f4462d7d0c3ed2d58386b
Amadeus Folego <amadeusfolego@gmail.com>
2012-09-27 11:17:50 -0300

    Completa reestruturação do xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,13 +1,13 @@
+import System.IO
 import XMonad
 import XMonad.Config.Desktop
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig
 import XMonad.Util.Run(spawnPipe)
-import System.IO
-import XMonad.Layout.PerWorkspace
 import qualified XMonad.StackSet as W
 
 main = do
@@ -25,59 +25,72 @@ main = do
         workspaces = myWorkspaces
       } `additionalKeysP` myKeys
 
-myWorkspaces = ["1:main","2:alt","3:web","4:email","5:chat","6:music","7","8","9"]
-
 myLayoutHook = avoidStruts $
-               spacing 3 $
+               spacing myBorderSpacing $
                smartBorders $
                layoutHook desktopConfig
 
 myLogHook h = dynamicLogWithPP $ xmobarPP {
-                ppTitle = xmobarColor "green" "" . shorten 50,
-                ppOutput = hPutStrLn h,
-                ppLayout = const ""
-              }
+  ppTitle = xmobarColor "green" "" . shorten 50,
+  ppOutput = hPutStrLn h,
+  ppLayout = const ""
+}
 
 myStartupHook :: X ()
 myStartupHook = do
-          spawn "unclutter" -- Hide mouse when unnecessary
-          spawn "xcompmgr -c" -- Enables compositing
-          spawn "xsetroot -cursor_name left_ptr" -- Use decent cursor
-          spawn "killall trayer; \
-                \trayer --edge top --align right --expand true --width 5 \
-                \--transparent true --alpha 0 --tint 0x000000 --height 18"
+  spawn "xcompmgr -c" -- Enables compositing
+  spawn "xsetroot -cursor_name left_ptr" -- Use decent cursor
+  spawn "killall trayer; \
+        \trayer --edge top --align right --expand true --width 5 \
+        \--transparent true --alpha 0 --tint 0x000000 --height 18"
 
 myManageHook = composeAll [
-                 className =? "File Operation Progress" --> doFloat,
-                 appName   =? "Skype"                   --> doFloat,
-                 appName   =? "alsamixer"               --> doFloat,
-                 resource  =? "desktop_window"          --> doIgnore,
-                 className =? "Do"                      --> doIgnore,
-                 className =? "xmobar"                  --> doIgnore,
-                 className =? "trayer"                  --> doIgnore
-               ]
+    className =? "File Operation Progress" --> doCenterFloat,
+    className =? "Skype"                   --> doCenterFloat,
+    appName   =? "alsamixer"               --> doCenterFloat,
+    appName   =? "QuickTerminal"           --> doCenterFloat,
+    resource  =? "desktop_window"          --> doIgnore,
+    className =? "Do"                      --> doIgnore,
+    className =? "xmobar"                  --> doIgnore,
+    className =? "trayer"                  --> doIgnore
+  ]
 
 myKeys = [
-           ("M-u", spawn "firefox"),
-           ("M-f", spawn "uzbl-tabbed"),
-           ("M-S-q", spawn "gnome-session-quit"),
-           ("M-S-r", spawn "xmonad --recompile && xmonad --restart"),
-           ("M-v", spawn "urxvt -e vim"),
-           ("M-t", spawn "thunderbird"),
-           ("M-n", spawn "nautilus"),
-           ("M-s", spawn "urxvt -e ncmpcpp"),
-           ("M-a", spawn "urxvt -name alsamixer -geometry 80x24 -e alsamixer" ),
-           ("M-v", spawn "urxvt -e vim"),
-           ("M-i", spawn "urxvt -name IRC -e ./.irc-script/irssi-connect.sh"),
-           ("M-g", spawn "gvim"),
-           ("M-p", spawn "gnome-do"),
-           ("M-<Return>", spawn "urxvt -name Terminal"),
-           ("M-b", sendMessage ToggleStruts),
-           ("M-S-t", withFocused $ windows . W.sink),
-           ("M-m", spawn "urxvt -e mutt"),
-           ("M-S-m", spawn "urxvt -e mutt -e \"source ~/.mutt/codeminer.account\""),
-           ("C-M1-f", spawn "mpc next"),
-           ("C-M1-r", spawn "mpc prev"),
-           ("C-M1-p", spawn "mpc pause"),
-           ("C-M1-S-p", spawn "mpc play")
-         ]
+    (         "M-a", spawn "urxvt -name alsamixer -geometry 70x30 -e alsamixer" ),
+    (         "M-b", sendMessage ToggleStruts),
+    (         "M-d", kill),
+    (         "M-f", withFocused $ windows . W.sink),
+    (      "C-M1-f", spawn "mpc next"),
+    (         "M-g", spawn "gvim"),
+    (       "M-S-g", spawn "urxvt -name QuickTerminal -e vim"),
+    (         "M-i", spawn "urxvt -name IRC -e bin/irssi-connect.sh"),
+    (         "M-m", spawn "urxvt -e mutt"),
+    (       "M-S-m", spawn "urxvt -e mutt -e \"source ~/.mutt/codeminer.account\""),
+    (         "M-n", spawn "nautilus"),
+    (         "M-p", spawn "gnome-do"),
+    (      "C-M1-p", spawn "mpc pause"),
+    (    "C-M1-S-p", spawn "mpc play"),
+    (       "M-S-q", spawn "gnome-session-quit"),
+    (       "M-S-r", spawn "xmonad --recompile && xmonad --restart"),
+    (      "C-M1-r", spawn "mpc prev"),
+    (         "M-s", spawn "urxvt -e ncmpcpp"),
+    (         "M-t", spawn "thunderbird"),
+    (         "M-u", spawn "uzbl-tabbed"),
+    (       "M-S-u", spawn "firefox"),
+    (         "M-v", spawn "urxvt -e vim"),
+    (         "M-x", spawn "gnome-control-center"),
+    (  "M-<Return>", spawn "urxvt -name Terminal"),
+    ("M-S-<Return>", spawn "urxvt -name QuickTerminal")
+  ]
+
+myWorkspaces = ["1:main",
+                "2:alt",
+                "3:web",
+                "4:email",
+                "5:chat",
+                "6:music",
+                "7",
+                "8",
+                "9"]
+
+myBorderSpacing = 3
