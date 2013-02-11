b9c61a4fa03b2b62937e78f974c79b87158c9253
Amadeus Folego <amadeusfolego@gmail.com>
2013-02-11 13:58:47 -0200

    Updates some xmonad settings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,7 +5,6 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
-import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig
 import XMonad.Util.Run(spawnPipe)
 import qualified XMonad.StackSet as W
@@ -14,8 +13,7 @@ main = do
       xmproc <- spawnPipe "xmobar"
       xmonad $ desktopConfig {
         normalBorderColor = "#000",
-        focusedBorderColor = "#68e862",
-        borderWidth = 1,
+        focusedBorderColor = "#111",--"#68e862",
         terminal = "urxvt",
         modMask = mod4Mask,
         manageHook = manageDocks <+> myManageHook <+> manageHook desktopConfig,
@@ -26,8 +24,7 @@ main = do
       } `additionalKeysP` myKeys
 
 myLayoutHook = avoidStruts $
-               spacing myBorderSpacing $
-               smartBorders $
+               noBorders $
                layoutHook desktopConfig
 
 myLogHook h = dynamicLogWithPP $ xmobarPP {
@@ -45,36 +42,38 @@ myStartupHook = do
         \--transparent true --alpha 0 --tint 0x000000 --height 18"
 
 myManageHook = composeAll [
-    className =? "File Operation Progress" --> doCenterFloat,
-    appName   =? "alsamixer"               --> doCenterFloat,
-    appName   =? "QuickTerminal"           --> doCenterFloat,
-    appName   =? "Uzbl WebInspector"       --> doCenterFloat,
-    resource  =? "desktop_window"          --> doIgnore,
-    className =? "Do"                      --> doIgnore,
-    className =? "xmobar"                  --> doIgnore,
-    className =? "trayer"                  --> doIgnore
-  ]
+      className =? "File Operation Progress" --> doCenterFloat,
+      appName   =? "alsamixer"               --> doCenterFloat,
+      appName   =? "QuickTerminal"           --> doCenterFloat,
+      appName   =? "Uzbl WebInspector"       --> doCenterFloat,
+      resource  =? "desktop_window"          --> doIgnore,
+      className =? "Do"                      --> doIgnore,
+      appName   =? "Synapse"                 --> doIgnore,
+      className =? "xmobar"                  --> doIgnore,
+      className =? "trayer"                  --> doIgnore
+    ]
 
 myKeys = [
     (         "M-a", spawn "urxvt -name alsamixer -geometry 70x30 -e alsamixer" ),
     (         "M-b", sendMessage ToggleStruts),
+    (         "M-c", spawn "chromium-browser"),
     (         "M-d", kill),
     (         "M-f", withFocused $ windows . W.sink),
     (      "C-M1-f", spawn "mpc next"),
     (         "M-g", spawn "gvim"),
     (       "M-S-g", spawn "urxvt -name QuickTerminal -e vim"),
-    (         "M-i", spawn "urxvt -name IRC -e bin/irssi-connect.sh"),
+    (         "M-i", spawn "urxvt -fn \"xft:Anonymous Pro:pixelsize=20\" -name IRC -e bin/irssi-connect.sh"),
     (         "M-m", spawn "urxvt -e mutt"),
     (       "M-S-m", spawn "urxvt -e mutt -e \"source ~/.mutt/codeminer.account\""),
     (         "M-n", spawn "urxvt -name QuickTerminal -e vifm"),
-    (       "M-S-n", spawn "vifm"),
-    (         "M-p", spawn "gnome-do"),
+    (       "M-S-n", spawn "nautilus"),
+    (         "M-p", spawn "synapse"),
     (      "C-M1-p", spawn "mpc pause"),
     (    "C-M1-S-p", spawn "mpc play"),
     (       "M-S-q", spawn "gnome-session-quit"),
     (       "M-S-r", spawn "xmonad --recompile && xmonad --restart"),
     (      "C-M1-r", spawn "mpc prev"),
-    (         "M-s", spawn "urxvt -e ncmpcpp"),
+    (         "M-s", spawn "urxvt -fn \"xft:Anonymous Pro:pixelsize=18\" -e ncmpcpp"),
     (         "M-t", spawn "thunderbird"),
     (         "M-u", spawn "uzbl-tabbed"),
     (       "M-S-u", spawn "firefox"),
@@ -85,15 +84,4 @@ myKeys = [
     ("M-S-<Return>", spawn "urxvt -name QuickTerminal")
   ]
 
-myWorkspaces = ["yī",
-                "èr",
-                "sān",
-                "sì",
-                "wǔ",
-                "liù",
-                "qī",
-                "bā",
-                "jiǔ",
-                "shí"]
-
-myBorderSpacing = 3
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
